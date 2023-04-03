05e26f4e6b0c7ed7b60cebb33a9b01efb1181652
FelixTheSapiens <114573071+FelixTheSapiens@users.noreply.github.com>
2023-04-03 23:23:27 +0400

    fix images

--- src/modules/PowerUp.js
+++ src/modules/PowerUp.js
@@ -42,7 +42,7 @@ function PowerUp(props) {
     <div className={`powerUp ${locked && 'powerUpDisabled'} ${affordable ? 'affordable' : 'pricy'}`} onClick={tryBuy}>
       <h3>{config.name}</h3>
       <div>
-        <img src={fingerGuns} />
+        <img src={img[config.img]} />
         <p>{config.desc}</p>
       </div>
       <div className='prices'>
